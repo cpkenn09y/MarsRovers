83662779544daeaa3087b9311e17fbfa8c256d35
Kenneth Yu <kenyu@ucdavis.edu>
2014-01-07 13:48:14 -0800

    refactored mars rover tests to include contexts

--- mars_rover_spec.rb
+++ mars_rover_spec.rb
@@ -3,61 +3,75 @@ require_relative 'mars_rover'
 
 describe "MarsRover" do
 
-  my_rover_north = MarsRover.new('1 2 N')
-  my_rover_south = MarsRover.new('1 2 S')
-  my_rover_east = MarsRover.new('1 2 E')
-  my_rover_west = MarsRover.new('1 2 W')
+  before(:each) {
+    @my_rover_north = MarsRover.new('1 2 N')
+    @my_rover_south = MarsRover.new('1 2 S')
+    @my_rover_east = MarsRover.new('1 2 E')
+    @my_rover_west = MarsRover.new('1 2 W')
+  }
 
-  it "has a starting location" do
-    expect(my_rover_north.location).to eq('1 2')
+  context "Initialization:" do
+
+    it "has a starting location" do
+      expect(@my_rover_north.location).to eq('1 2')
+    end
+
+    it "has a directional orientation" do
+      expect(@my_rover_north.orientation).to eq('N')
+    end
+
+    it "has a status which contains location and directional orientation" do
+      expect(@my_rover_north.status).to eq('1 2 N')
+    end
+
   end
 
-  it "has a starting directional orientation" do
-    expect(my_rover_north.orientation).to eq('N')
+  context 'Moves Forward:' do
+
+    it "when facing North" do
+      @my_rover_north.move_forward
+      expect(@my_rover_north.get_status).to eq('1 3 N')
+    end
+
+    it "when facing South" do
+      @my_rover_south.move_forward
+      expect(@my_rover_south.get_status).to eq('1 1 S')
+    end
+
+    it "when facing East" do
+      @my_rover_east.move_forward
+      expect(@my_rover_east.get_status).to eq('2 2 E')
+    end
+
+    it "when facing West" do
+      @my_rover_west.move_forward
+      expect(@my_rover_west.get_status).to eq('0 2 W')
+    end
+
   end
 
-  it "outputs its status: location and directional orientation" do
-    expect(my_rover_north.status).to eq('1 2 N')
-  end
+  context "Right Turn:" do
 
-  it "is able to move forward when facing North" do
-    my_rover_north.move_forward
-    expect(my_rover_north.get_status).to eq('1 3 N')
-  end
+    it "changes orientation from North to East" do
+      @my_rover_north.turn_right
+      expect(@my_rover_north.orientation).to eq('E')
+    end
 
-  it "is able to move forward when facing South" do
-    my_rover_south.move_forward
-    expect(my_rover_south.get_status).to eq('1 1 S')
-  end
+    it "changes orientation from South to West" do
+      @my_rover_south.turn_right
+      expect(@my_rover_south.orientation).to eq('W')
+    end
 
-  it "is able to move forward when facing East" do
-    my_rover_east.move_forward
-    expect(my_rover_east.get_status).to eq('2 2 E')
-  end
+    it "changes orientation from East to South" do
+      @my_rover_east.turn_right
+      expect(@my_rover_east.orientation).to eq('S')
+    end
 
-  it "is able to move forward when facing West" do
-    my_rover_west.move_forward
-    expect(my_rover_west.get_status).to eq('0 2 W')
-  end
+    it "changes orientation from West to North" do
+      @my_rover_west.turn_right
+      expect(@my_rover_west.orientation).to eq('N')
+    end
 
-  it "is able to turn right when facing N" do
-    my_rover_north.turn_right
-    expect(my_rover_north.orientation).to eq('E')
-  end
-
-  it "is able to turn right when facing S" do
-    my_rover_south.turn_right
-    expect(my_rover_south.orientation).to eq('W')
-  end
-
-  it "is able to turn right when facing E" do
-    my_rover_east.turn_right
-    expect(my_rover_east.orientation).to eq('S')
-  end
-
-  it "is able to turn right when facing W" do
-    my_rover_west.turn_right
-    expect(my_rover_west.orientation).to eq('N')
   end
 
 end
