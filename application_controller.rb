2b331ab7fa29e85aae3c8387700d81f38b14a90b
Kenneth Yu <kenyu@ucdavis.edu>
2014-01-10 18:24:39 -0800

    ApplicationController is able to accept user input and run whole program
    * the Grid is printed to the terminal with the Rovers at thie initial position
    * also prints the Grid with the Rovers in their final position
    * Tests all day!

---
+++ application_controller.rb
@@ -0,0 +1,162 @@
+require_relative 'mars_rover'
+require_relative 'rover_command_parser'
+require_relative 'grid'
+
+class ApplicationController
+
+  @parsed_command = nil
+  @setup_data = nil
+  @current_grid = nil
+  @rovers_movement_data = nil
+  @rovers = nil
+
+  def self.parse_and_execute(user_input)
+    parse_command(user_input)
+
+    set_setup_data
+    set_rovers_movement_data
+    rovers_starting_configurations = get_starting_configurations
+
+    create_rovers(rovers_starting_configurations)
+
+    grid_dimensions = get_grid_dimension
+    create_grid(grid_dimensions)
+
+    puts "INITIAL POSITION OF ROVERS:"
+    placeRoversOnGrid
+
+
+    deliver_rover_commands
+    reset_grid
+
+
+    puts "FINAL POSITION OF ROVERS:"
+    placeRoversOnGrid
+    rovers_status = get_rovers_status
+
+
+    puts "- - - - - - - - - - - - - - - - -"
+    puts "FINAL OUTPUT:"
+    puts rovers_status
+    rovers_status
+  end
+
+  def self.parse_command(user_input)
+    @parsed_command = RoverCommandParser.assess(user_input)
+  end
+
+  def self.set_setup_data
+    @setup_data = @parsed_command[:setup_data]
+  end
+
+  def self.get_grid_dimension
+    RoverCommandParser.get_grid_dimension(@setup_data)
+  end
+
+  def self.get_number_of_rovers
+    @setup_data[:number_of_rovers]
+  end
+
+  def self.set_rovers_movement_data
+    @rovers_movement_data = @parsed_command[:rovers_movement_data]
+  end
+
+  def self.create_grid(grid_dimensions)
+    @current_grid = Grid.new(grid_dimensions)
+  end
+
+  def self.get_starting_configurations
+    @rovers_movement_data.map {|rover| rover[:rover_movement_data][:starting_position]}
+  end
+
+  def self.create_rovers(rovers_starting_configurations)
+    rovers = []
+    rovers_starting_configurations.each {|starting_configuration| rovers << MarsRover.new(starting_configuration)}
+    @rovers = rovers
+  end
+
+  def self.placeRoversOnGrid
+
+    @rovers.each_with_index do |rover, index|
+      avatar = (index + 1).to_s
+      target_location = rover.location
+      @current_grid.place_onto_grid(avatar, target_location, rover.orientation)
+    end
+
+    @current_grid.print
+  end
+
+  def self.reset_grid
+    @current_grid.reset
+  end
+
+  def self.deliver_rover_commands
+    rovers_commands = get_rovers_commands
+
+    @rovers.each_with_index do |rover, index|
+      rover.perform_commands(rovers_commands[index])
+    end
+
+  end
+
+  def self.get_rovers_commands
+    @rovers_movement_data.map {|rover| rover[:rover_movement_data][:commands]}
+  end
+
+  def self.get_rovers_status
+    @rovers.map { |rover| rover.status}
+  end
+
+end
+
+user_input_given =
+"5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM"
+
+ApplicationController.parse_and_execute(user_input_given)
+
+
+
+### Uncomment Lines 125-134 to run the application with three rovers :) ###
+
+# user_input_3_rovers =
+# "4 4
+# 3 2 N
+# LMML
+# 3 3 W
+# MMR
+# 4 2 S
+# MRR"
+
+# ApplicationController.parse_and_execute(user_input_3_rovers)
+
+
+### Uncomment Lines 141-160 to run the application with eight rovers ###
+
+=begin
+
+user_input_8_rovers =
+"8 8
+3 4 N
+MRM
+2 1 E
+LMMR
+5 5 S
+MML
+4 4 N
+MMRM
+5 2 E
+MMR
+0 0 N
+MMRMLL
+7 7 S
+MMRMM
+6 1 W
+MMRMML"
+
+ApplicationController.parse_and_execute(user_input_8_rovers)
+
+=end
---
+++ application_controller_spec.rb
@@ -0,0 +1,124 @@
+require 'rspec'
+require_relative 'application_controller'
+
+describe "ApplicationController" do
+
+user_input =
+"5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM"
+
+  before(:each) do
+    @parsed_command = ApplicationController.parse_command(user_input)
+    @setup_data = ApplicationController.set_setup_data
+    @rovers_movement_data = ApplicationController.set_rovers_movement_data
+    @rovers_starting_configurations = ApplicationController.get_starting_configurations
+    @rovers = ApplicationController.create_rovers(@rovers_starting_configurations)
+  end
+
+  context "Parse Command:" do
+    it "returns set up data and rovers movement data" do
+      expect(@parsed_command).to eq(
+        {
+          :setup_data =>
+            {
+              grid_dimension: "5 5",
+              number_of_rovers: 2
+            },
+          :rovers_movement_data =>
+            [
+              {:rover_movement_data => {starting_position: "1 2 N", commands: "LMLMLMLMM" }},
+              {:rover_movement_data => {starting_position: "3 3 E", commands: "MMRMMRMRRM"}}
+            ]
+        }
+      )
+    end
+  end
+
+  context "Set Up Data:" do
+    it "extracts set up data out of the parsed command" do
+      expect(@setup_data).to eq(
+        {:grid_dimension => "5 5", :number_of_rovers => 2}
+      )
+    end
+  end
+
+  context "Get Grid Dimension:" do
+    it "return a hash of x_max and y_max" do
+      expect(ApplicationController.get_grid_dimension).to eq(
+        {:x_max => 5, :y_max => 5}
+      )
+    end
+  end
+
+  context "Get Number of Rovers:" do
+
+    it "calculates number of rovers based on set up data" do
+      expect(ApplicationController.get_number_of_rovers).to eq(2)
+    end
+
+  end
+
+  context "Rovers Movement Data:" do
+
+    it "returns an array of rover movement data hashes" do
+      expect(@rovers_movement_data).to eq(
+        [
+          {
+            :rover_movement_data => { starting_position: "1 2 N", commands: "LMLMLMLMM"}
+          },
+          {
+            :rover_movement_data => {starting_position: "3 3 E", :commands=>"MMRMMRMRRM"}
+          }
+        ]
+      )
+    end
+
+  end
+
+  context "Get Starting Configurations:" do
+
+    it "returns an array with starting position of rovers" do
+      expect(@rovers_starting_configurations).to eq(["1 2 N", "3 3 E"])
+    end
+
+  end
+
+  context "Deliver Rover Commands:" do
+
+    it "provides each rover with its commands, leading to the execution of said commands" do
+      ApplicationController.deliver_rover_commands
+      rover_1_status = @rovers[0].status
+      expect(rover_1_status).to eq("1 3 N")
+    end
+
+  end
+
+  context "Parse and Execute:" do
+    it "runs user input through parser and has rovers execute commands" do
+      expect(ApplicationController.parse_and_execute(user_input)).to eq(["1 3 N", "5 1 E"])
+    end
+  end
+
+end
+
+=begin
+
+Rover will receive commands, and then output its current location and its orientation.
+
+Able to move multiple rovers.
+
+INPUT:
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+
+OUTPUT:
+1 3 N
+5 1 E
+
+=end
