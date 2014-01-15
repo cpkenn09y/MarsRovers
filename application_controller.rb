7da099d257bece0527366c4e02ce0f5b79fc3ec0
Kenneth Yu <kenyu@ucdavis.edu>
2014-01-15 14:41:28 -0800

    refactored class methods

--- application_controller.rb
+++ application_controller.rb
@@ -4,107 +4,111 @@ require_relative 'grid'
 
 class ApplicationController
 
-  @parsed_command = nil
-  @setup_data = nil
-  @current_grid = nil
-  @rovers_movement_data = nil
-  @rovers = nil
+  class << self
 
-  def self.parse_and_execute(user_input)
-    parse_command(user_input)
+    @parsed_command = nil
+    @setup_data = nil
+    @current_grid = nil
+    @rovers_movement_data = nil
+    @rovers = nil
 
-    set_setup_data
-    set_rovers_movement_data
-    rovers_starting_configurations = get_starting_configurations
+    def parse_and_execute(user_input)
+      parse_command(user_input)
 
-    create_rovers(rovers_starting_configurations)
+      set_setup_data
+      set_rovers_movement_data
+      rovers_starting_configurations = get_starting_configurations
 
-    grid_dimensions = get_grid_dimension
-    create_grid(grid_dimensions)
+      create_rovers(rovers_starting_configurations)
 
-    puts "INITIAL POSITION OF ROVERS:"
-    placeRoversOnGrid
+      grid_dimensions = get_grid_dimension
+      create_grid(grid_dimensions)
 
-
-    deliver_rover_commands
-    reset_grid
+      puts "INITIAL POSITION OF ROVERS:"
+      placeRoversOnGrid
 
 
-    puts "FINAL POSITION OF ROVERS:"
-    placeRoversOnGrid
-    rovers_status = get_rovers_status
+      deliver_rover_commands
+      reset_grid
 
 
-    puts "- - - - - - - - - - - - - - - - -"
-    puts "FINAL OUTPUT:"
-    puts rovers_status
-    rovers_status
-  end
+      puts "FINAL POSITION OF ROVERS:"
+      placeRoversOnGrid
+      rovers_status = get_rovers_status
 
-  def self.parse_command(user_input)
-    @parsed_command = RoverCommandParser.assess(user_input)
-  end
 
-  def self.set_setup_data
-    @setup_data = @parsed_command[:setup_data]
-  end
-
-  def self.get_grid_dimension
-    RoverCommandParser.get_grid_dimension(@setup_data)
-  end
-
-  def self.get_number_of_rovers
-    @setup_data[:number_of_rovers]
-  end
-
-  def self.set_rovers_movement_data
-    @rovers_movement_data = @parsed_command[:rovers_movement_data]
-  end
-
-  def self.create_grid(grid_dimensions)
-    @current_grid = Grid.new(grid_dimensions)
-  end
-
-  def self.get_starting_configurations
-    @rovers_movement_data.map {|rover| rover[:rover_movement_data][:starting_position]}
-  end
-
-  def self.create_rovers(rovers_starting_configurations)
-    rovers = []
-    rovers_starting_configurations.each {|starting_configuration| rovers << MarsRover.new(starting_configuration)}
-    @rovers = rovers
-  end
-
-  def self.placeRoversOnGrid
-
-    @rovers.each_with_index do |rover, index|
-      avatar = (index + 1).to_s
-      target_location = rover.location
-      @current_grid.place_onto_grid(avatar, target_location, rover.orientation)
+      puts "- - - - - - - - - - - - - - - - -"
+      puts "FINAL OUTPUT:"
+      puts rovers_status
+      rovers_status
     end
 
-    @current_grid.print
-  end
-
-  def self.reset_grid
-    @current_grid.reset
-  end
-
-  def self.deliver_rover_commands
-    rovers_commands = get_rovers_commands
-
-    @rovers.each_with_index do |rover, index|
-      rover.perform_commands(rovers_commands[index])
+    def parse_command(user_input)
+      @parsed_command = RoverCommandParser.assess(user_input)
     end
 
-  end
+    def set_setup_data
+      @setup_data = @parsed_command[:setup_data]
+    end
 
-  def self.get_rovers_commands
-    @rovers_movement_data.map {|rover| rover[:rover_movement_data][:commands]}
-  end
+    def get_grid_dimension
+      RoverCommandParser.get_grid_dimension(@setup_data)
+    end
 
-  def self.get_rovers_status
-    @rovers.map { |rover| rover.status}
+    def get_number_of_rovers
+      @setup_data[:number_of_rovers]
+    end
+
+    def set_rovers_movement_data
+      @rovers_movement_data = @parsed_command[:rovers_movement_data]
+    end
+
+    def create_grid(grid_dimensions)
+      @current_grid = Grid.new(grid_dimensions)
+    end
+
+    def get_starting_configurations
+      @rovers_movement_data.map {|rover| rover[:rover_movement_data][:starting_position]}
+    end
+
+    def create_rovers(rovers_starting_configurations)
+      rovers = []
+      rovers_starting_configurations.each {|starting_configuration| rovers << MarsRover.new(starting_configuration)}
+      @rovers = rovers
+    end
+
+    def placeRoversOnGrid
+
+      @rovers.each_with_index do |rover, index|
+        avatar = (index + 1).to_s
+        target_location = rover.location
+        @current_grid.place_onto_grid(avatar, target_location, rover.orientation)
+      end
+
+      @current_grid.print
+    end
+
+    def reset_grid
+      @current_grid.reset
+    end
+
+    def deliver_rover_commands
+      rovers_commands = get_rovers_commands
+
+      @rovers.each_with_index do |rover, index|
+        rover.perform_commands(rovers_commands[index])
+      end
+
+    end
+
+    def get_rovers_commands
+      @rovers_movement_data.map {|rover| rover[:rover_movement_data][:commands]}
+    end
+
+    def get_rovers_status
+      @rovers.map { |rover| rover.status}
+    end
+
   end
 
 end
--- rover_command_parser.rb
+++ rover_command_parser.rb
@@ -1,59 +1,63 @@
 module RoverCommandParser
 
-  def self.clean_user_input(user_input)
-    user_input.split("\n").map {|input| input.strip}
-  end
+  class << self
 
-  def self.extract_setup_data(cleaned_input)
-    {:grid_dimension => cleaned_input[0], :number_of_rovers => cleaned_input.length / 2}
-  end
-
-  def self.get_grid_dimension(setup_data)
-    extract_x_max_y_max(setup_data)
-  end
-
-  def self.extract_x_max_y_max(setup_data)
-    raw_dimensions = setup_data[:grid_dimension]
-    dimensions_separated = raw_dimensions.split(' ')
-    {x_max: dimensions_separated[0].to_i, y_max: dimensions_separated[1].to_i}
-  end
-
-  def self.get_number_of_rovers(setup_data)
-    setup_data[:number_of_rovers]
-  end
-
-  def self.extract_rovers_raw_data(cleaned_input)
-    cleaned_input[1..-1]
-  end
-
-  def self.create_rover_hashes(rovers_data, number_of_rovers)
-
-    rovers = []
-
-    0.upto(number_of_rovers-1) do |i|
-      rovers.push(:rover_movement_data => {
-        :starting_position => rovers_data[i*2],
-        :commands => rovers_data[i*2+1]
-      })
+    def clean_user_input(user_input)
+      user_input.split("\n").map {|input| input.strip}
     end
 
-    rovers
-  end
+    def extract_setup_data(cleaned_input)
+      {:grid_dimension => cleaned_input[0], :number_of_rovers => cleaned_input.length / 2}
+    end
 
-  def self.get_starting_position(rover_hash)
-    rover_hash[0][:rover_movement_data][:starting_position]
-  end
+    def get_grid_dimension(setup_data)
+      extract_x_max_y_max(setup_data)
+    end
 
-  def self.assess(user_input)
-    cleaned_input = clean_user_input(user_input)
+    def extract_x_max_y_max(setup_data)
+      raw_dimensions = setup_data[:grid_dimension]
+      dimensions_separated = raw_dimensions.split(' ')
+      {x_max: dimensions_separated[0].to_i, y_max: dimensions_separated[1].to_i}
+    end
 
-    setup_data = extract_setup_data(cleaned_input)
-    number_of_rovers = setup_data[:number_of_rovers]
+    def get_number_of_rovers(setup_data)
+      setup_data[:number_of_rovers]
+    end
 
-    rovers_raw_data = extract_rovers_raw_data(cleaned_input)
-    rovers_hash = create_rover_hashes(rovers_raw_data, number_of_rovers)
+    def extract_rovers_raw_data(cleaned_input)
+      cleaned_input[1..-1]
+    end
 
-    {setup_data: setup_data, rovers_movement_data: rovers_hash}
+    def create_rover_hashes(rovers_data, number_of_rovers)
+
+      rovers = []
+
+      0.upto(number_of_rovers-1) do |i|
+        rovers.push(:rover_movement_data => {
+          :starting_position => rovers_data[i*2],
+          :commands => rovers_data[i*2+1]
+        })
+      end
+
+      rovers
+    end
+
+    def get_starting_position(rover_hash)
+      rover_hash[0][:rover_movement_data][:starting_position]
+    end
+
+    def assess(user_input)
+      cleaned_input = clean_user_input(user_input)
+
+      setup_data = extract_setup_data(cleaned_input)
+      number_of_rovers = setup_data[:number_of_rovers]
+
+      rovers_raw_data = extract_rovers_raw_data(cleaned_input)
+      rovers_hash = create_rover_hashes(rovers_raw_data, number_of_rovers)
+
+      {setup_data: setup_data, rovers_movement_data: rovers_hash}
+    end
+
   end
 
 end
